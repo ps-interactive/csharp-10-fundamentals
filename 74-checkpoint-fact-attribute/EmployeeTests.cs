d794a825cf1047f7513ec6f35126412acf7b20c4
Gill Cleeren <GillCleeren@users.noreply.github.com>
2022-07-20 22:13:42 +0200

    74 - fix test

---
+++ 74-checkpoint-fact-attribute/EmployeeTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+public class VehicleTests
+{
+    public void Drive_Adds_Miles()
+    {
+        Vehicle vehicle = new Vehicle(10, "BMW", "Black");
+
+        vehicle.Drive();
+
+        
+    }
+}
