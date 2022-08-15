244a7fc58c20dd9610f0936e92d6d9ae3b4ad353
Gill Cleeren <GillCleeren@users.noreply.github.com>
2022-08-15 12:10:26 +0200

    Ex03

--- 03-checkpoint-int-variable/Program.cs
+++ 03-checkpoint-int-variable/Program.cs
@@ -1,1 +1,7 @@
-﻿
+﻿Console.WriteLine("Enter a first number:");
+string stringValue1 = Console.ReadLine();
+Console.WriteLine("Enter a second number:");
+string stringValue2 = Console.ReadLine();
+
+int intValue1 = int.Parse(stringValue1);
+int intValue2 = int.Parse(stringValue2);
