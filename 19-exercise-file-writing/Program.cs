3afa7eba8364742900846062d58e2cc505944ff6
Gill Cleeren <GillCleeren@users.noreply.github.com>
2022-09-04 16:21:27 +0200

    BMW

--- 19-exercise-file-writing/Program.cs
+++ 19-exercise-file-writing/Program.cs
@@ -1,6 +1,6 @@
 ﻿
 
-Vehicle v1 = new Vehicle(10, "BMW", "Red");
+Vehicle v1 = new Vehicle(10, "Ford", "Red");
 Vehicle v2 = new Vehicle(45, "Toyota", "Blue");
 Vehicle v3 = new Vehicle(987, "Renault", "Black");
 Vehicle v4 = new Vehicle(45643, "Volkswagen", "Green");
